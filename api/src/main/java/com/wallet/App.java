d7cc6920984f468f2872cef83e34c15a74be6eeb
DESKTOP-9EI3EKO\Kostas <test>
2020-12-28 14:18:38 +0000

    Create handlers for the endpoints

--- api/src/main/java/com/wallet/App.java
+++ api/src/main/java/com/wallet/App.java
@@ -1,8 +1,17 @@
 package com.wallet;
 
+import ratpack.server.RatpackServer;
+
 public class App {
 
-    public static void main(String[] args) {
-        //TODO: Ratpack handlers
+    public static void main(String[] args) throws Exception {
+        RatpackServer.start(ratpackServerSpec -> ratpackServerSpec
+            .handlers(chain -> chain
+                .post("login", ctx -> ctx.render("Login"))
+                .get("transcations", ctx -> ctx.render("Transactions"))
+                .get("balance", ctx -> ctx.render("Balance"))
+                .post("spend", ctx -> ctx.render("Spend"))
+
+            ));
     }
 }
