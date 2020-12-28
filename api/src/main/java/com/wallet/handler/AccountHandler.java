f9fb9b682d24775ef6b40a7bc237d2bb82c8081b
DESKTOP-9EI3EKO\Kostas <test>
2020-12-28 15:09:57 +0000

    Add allowed methods in each handler

--- api/src/main/java/com/wallet/handler/AccountHandler.java
+++ api/src/main/java/com/wallet/handler/AccountHandler.java
@@ -2,12 +2,18 @@ package com.wallet.handler;
 
 import ratpack.handling.Context;
 import ratpack.handling.InjectionHandler;
+import ratpack.http.Response;
 
 public class AccountHandler extends InjectionHandler {
 
     public void handle(Context ctx, String base) throws Exception {
+        Response response = ctx.getResponse();
 
         ctx.byMethod(byMethodSpec -> byMethodSpec
+                .options(() -> {
+                    response.getHeaders().set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, DELETE");
+                    response.send();
+                })
                 .get(() -> {
                     ctx.render("Balance");
                 })
