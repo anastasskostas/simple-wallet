47e02e462503a082bf7a43ace2ff382c3743ffc9
DESKTOP-9EI3EKO\Kostas <test>
2020-12-28 15:51:49 +0000

    Implement balance endpoint and return user data

--- api/src/main/java/com/wallet/handler/AccountHandler.java
+++ api/src/main/java/com/wallet/handler/AccountHandler.java
@@ -1,13 +1,23 @@
 package com.wallet.handler;
 
+import com.google.gson.Gson;
+import com.wallet.authorization.JwtTokenUtil;
+import com.wallet.model.User;
+import com.wallet.storage.RedisPool;
 import ratpack.handling.Context;
 import ratpack.handling.InjectionHandler;
+import ratpack.http.Request;
 import ratpack.http.Response;
+import redis.clients.jedis.Jedis;
+
+import static ratpack.jackson.Jackson.json;
 
 public class AccountHandler extends InjectionHandler {
 
     public void handle(Context ctx, String base) throws Exception {
+        Gson gson = new Gson();
         Response response = ctx.getResponse();
+        Request request = ctx.getRequest();
 
         ctx.byMethod(byMethodSpec -> byMethodSpec
                 .options(() -> {
@@ -15,7 +25,16 @@ public class AccountHandler extends InjectionHandler {
                     response.send();
                 })
                 .get(() -> {
-                    ctx.render("Balance");
+                    try (Jedis jedis = RedisPool.getJedis()) {
+                        String token = request.getHeaders().get("Authorization");
+                        JwtTokenUtil jwtTokenUtil = new JwtTokenUtil();
+                        String uid = jwtTokenUtil.getUidFromToken(token);
+
+                        String userJson = jedis.get("user#" + uid);
+                        User user = gson.fromJson(userJson, User.class);
+
+                        ctx.render(json(user));
+                    }
                 })
                 .post(() -> {
                     ctx.render("Spend");
